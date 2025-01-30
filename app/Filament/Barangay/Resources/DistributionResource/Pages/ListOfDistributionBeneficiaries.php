c29d97a5d1479b0a41cfd536938b40937b75e9de
Brian Orbino <86046953+orbitsdev@users.noreply.github.com>
2025-01-30 16:42:21 +0800

    update add filter to beneficiaries record

--- app/Filament/Barangay/Resources/DistributionResource/Pages/ListOfDistributionBeneficiaries.php
+++ app/Filament/Barangay/Resources/DistributionResource/Pages/ListOfDistributionBeneficiaries.php
@@ -2,6 +2,7 @@
 
 namespace App\Filament\Barangay\Resources\DistributionResource\Pages;
 
+use Filament\Tables\Table;
 use App\Models\Beneficiary;
 use App\Models\Distribution;
 use Filament\Resources\Pages\Page;
@@ -13,13 +14,15 @@ use Filament\Tables\Columns\ViewColumn;
 use Filament\Tables\Contracts\HasTable;
 use Illuminate\Database\Eloquent\Model;
 use Filament\Notifications\Notification;
+use Filament\Tables\Enums\FiltersLayout;
 use Filament\Tables\Filters\SelectFilter;
 use Filament\Forms\Concerns\InteractsWithForms;
 use Filament\Tables\Concerns\InteractsWithTable;
+
 use App\Filament\Barangay\Resources\DistributionResource;
 use Filament\Resources\Pages\Concerns\InteractsWithRecord;
 
-use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
 
 class ListOfDistributionBeneficiaries extends Page  implements HasForms, HasTable
 
@@ -33,15 +36,12 @@ class ListOfDistributionBeneficiaries extends Page  implements HasForms, HasTabl
 
     public $record;
 
-    public function mount(Distribution $record): void
-    {
-
-    }
+    public function mount(Distribution $record): void {}
 
     public function getHeading(): string
     {
         $item = $this->record->title;
-        return __($item.' Benificiaries');
+        return __($item . ' Benificiaries');
     }
 
     public function table(Table $table): Table
@@ -53,117 +53,120 @@ class ListOfDistributionBeneficiaries extends Page  implements HasForms, HasTabl
                 TextColumn::make('name')->searchable(),
                 TextColumn::make('email')->searchable(),
                 TextColumn::make('contact')->searchable(),
+                TextColumn::make('distributionItem.item.name')->searchable(),
                 TextColumn::make('status')
-                ->badge()
-                ->color(fn(string $state): string => match ($state) {
-                    Beneficiary::CLAIMED => 'success',
+                    ->badge()
+                    ->color(fn(string $state): string => match ($state) {
+                        Beneficiary::CLAIMED => 'success',
 
-                    default => 'gray'
-                }),
+                        default => 'gray'
+                    }),
             ])
             ->filters([
                 SelectFilter::make('status')
-                ->options(Beneficiary::STATUS_OPTIONS)->searchable()
-            ])
-            ->headerActions([
+                    ->options(Beneficiary::STATUS_OPTIONS)->searchable(),
+                // SelectFilter::make('distribution_item_id')
+                //     ->relationship(
+                //         'distributionItem',
+                //         fn(Builder $query) => $query,
+                //     )
+                //     ->getOptionLabelFromRecordUsing(fn (Model $record) => "{$record->created_at}")
 
-            ])
+            ], layout: FiltersLayout::AboveContent)
+            ->headerActions([])
             ->actions([
                 Action::make('Claim')
-                ->requiresConfirmation()
-                ->button()
-                ->action(function (Model $beneficiary) {
-                    // Update beneficiary status to 'Claimed'
-                    $beneficiary->update(['status' => Beneficiary::CLAIMED]);
+                    ->requiresConfirmation()
+                    ->button()
+                    ->action(function (Model $beneficiary) {
+                        // Update beneficiary status to 'Claimed'
+                        $beneficiary->update(['status' => Beneficiary::CLAIMED]);
 
-                    // Decrease the quantity in the associated distribution item
-                    $distributionItem = $beneficiary->distributionItem;
-                    if ($distributionItem->quantity > 0) {
-                        $distributionItem->decrement('quantity');
-                    }
-                    $adminId = Auth::user()->role === 'admin' ? Auth::user()->id : null;
-                    // Create a transaction record
-                    $beneficiary->transactions()->create([
-                        'barangay_id' => $distributionItem->distribution->barangay_id,
-                        'barangay_name' => $distributionItem->distribution->barangay->name,
-                        'barangay_location' => $distributionItem->distribution->barangay->location,
+                        // Decrease the quantity in the associated distribution item
+                        $distributionItem = $beneficiary->distributionItem;
+                        if ($distributionItem->quantity > 0) {
+                            $distributionItem->decrement('quantity');
+                        }
+                        $adminId = Auth::user()->role === 'admin' ? Auth::user()->id : null;
+                        // Create a transaction record
+                        $beneficiary->transactions()->create([
+                            'barangay_id' => $distributionItem->distribution->barangay_id,
+                            'barangay_name' => $distributionItem->distribution->barangay->name,
+                            'barangay_location' => $distributionItem->distribution->barangay->location,
 
-                        'distribution_id' => $distributionItem->distribution_id,
-                        'distribution_title' => $distributionItem->distribution->title,
-                        'distribution_location' => $distributionItem->distribution->location,
-                        'distribution_date' => $distributionItem->distribution->distribution_date,
-                        'distribution_code' => $distributionItem->distribution->code,
+                            'distribution_id' => $distributionItem->distribution_id,
+                            'distribution_title' => $distributionItem->distribution->title,
+                            'distribution_location' => $distributionItem->distribution->location,
+                            'distribution_date' => $distributionItem->distribution->distribution_date,
+                            'distribution_code' => $distributionItem->distribution->code,
 
-                        'distribution_item_id' => $distributionItem->id,
-                        'distribution_item_name' => $distributionItem->item->name,
+                            'distribution_item_id' => $distributionItem->id,
+                            'distribution_item_name' => $distributionItem->item->name,
 
-                        'beneficiary_id' => $beneficiary->id,
-                        'beneficiary_name' => $beneficiary->name,
-                        'beneficiary_contact' => $beneficiary->contact,
-                        'beneficiary_email' => $beneficiary->email,
-                        'beneficiary_code' => $beneficiary->code,
-                       'admin_id' => $adminId,
+                            'beneficiary_id' => $beneficiary->id,
+                            'beneficiary_name' => $beneficiary->name,
+                            'beneficiary_contact' => $beneficiary->contact,
+                            'beneficiary_email' => $beneficiary->email,
+                            'beneficiary_code' => $beneficiary->code,
+                            'admin_id' => $adminId,
 
-                        // 'support_id' => $distributionItem->distribution->support_id,
-                        // 'support_name' => $distributionItem->distribution->support->personnel->user->name ?? null,
-                        // 'support_type' => $distributionItem->distribution->support->type ?? null,
-                        // 'support_unique_code' => $distributionItem->distribution->support->unique_code ?? null,
+                            // 'support_id' => $distributionItem->distribution->support_id,
+                            // 'support_name' => $distributionItem->distribution->support->personnel->user->name ?? null,
+                            // 'support_type' => $distributionItem->distribution->support->type ?? null,
+                            // 'support_unique_code' => $distributionItem->distribution->support->unique_code ?? null,
 
-                        'action' => 'Claimed',
-                        'performed_at' => now(),
-                    ]);
+                            'action' => 'Claimed',
+                            'performed_at' => now(),
+                        ]);
 
-                    Notification::make()
-                        ->title('Beneficiary Claimed Successfully')
-                        ->success()
-                        ->send();
-                })
-                ->hidden(function (Model $beneficiary) {
-                    return $beneficiary->status === Beneficiary::CLAIMED;
-                })
-                ->icon('far-hand-back-fist')
-                ->color('success'),
+                        Notification::make()
+                            ->title('Beneficiary Claimed Successfully')
+                            ->success()
+                            ->send();
+                    })
+                    ->hidden(function (Model $beneficiary) {
+                        return $beneficiary->status === Beneficiary::CLAIMED;
+                    })
+                    ->icon('far-hand-back-fist')
+                    ->color('success'),
                 Action::make('Unclaim')
-                ->requiresConfirmation()
-                ->button()
-                ->action(function (Model $beneficiary) {
-                    // Update beneficiary status to 'Unclaimed'
-                    $beneficiary->update(['status' => Beneficiary::UN_CLAIMED]);
+                    ->requiresConfirmation()
+                    ->button()
+                    ->action(function (Model $beneficiary) {
+                        // Update beneficiary status to 'Unclaimed'
+                        $beneficiary->update(['status' => Beneficiary::UN_CLAIMED]);
 
-                    // Increase the quantity in the associated distribution item
-                    $distributionItem = $beneficiary->distributionItem;
-                    $distributionItem->increment('quantity');
+                        // Increase the quantity in the associated distribution item
+                        $distributionItem = $beneficiary->distributionItem;
+                        $distributionItem->increment('quantity');
 
-                    // Delete the associated transaction for this beneficiary
-                    $beneficiary->transactions()
-                        ->where('action', 'Claimed')
-                        ->where('beneficiary_id', $beneficiary->id)
-                        ->delete();
+                        // Delete the associated transaction for this beneficiary
+                        $beneficiary->transactions()
+                            ->where('action', 'Claimed')
+                            ->where('beneficiary_id', $beneficiary->id)
+                            ->delete();
 
-                    Notification::make()
-                        ->title('Beneficiary Unclaimed Successfully')
-                        ->success()
-                        ->send();
-                })
-                ->hidden(function (Model $beneficiary) {
-                    return $beneficiary->status === Beneficiary::UN_CLAIMED;
-                })
-                ->icon('heroicon-o-x-circle')
-                ->color('gray')
-                ->label('Revert'),
-
-
+                        Notification::make()
+                            ->title('Beneficiary Unclaimed Successfully')
+                            ->success()
+                            ->send();
+                    })
+                    ->hidden(function (Model $beneficiary) {
+                        return $beneficiary->status === Beneficiary::UN_CLAIMED;
+                    })
+                    ->icon('heroicon-o-x-circle')
+                    ->color('gray')
+                    ->label('Revert'),
 
 
 
-            ])
-            ->bulkActions([
+
 
             ])
-            ->modifyQueryUsing(function($query){
+            ->bulkActions([])
+            ->modifyQueryUsing(function ($query) {
                 return $query;
             })
-            ;
+        ;
     }
-
 }
