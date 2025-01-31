9591baba44d71d0c24874d6b25b4c7e9dc4a4a47
Brian Orbino <86046953+orbitsdev@users.noreply.github.com>
2025-01-31 12:02:48 +0800

    wip

--- resources/views/exports/barangay-distributions.blade.php
+++ resources/views/exports/barangay-distributions.blade.php
@@ -1,25 +1,55 @@
 <table align="left">
     <thead>
         <tr style="background-color: #1d4ed8; color: white;">
-            <th align="left" width="40" style="background-color: #106c3b; color: white;">ID</th>
+            <th align="left" width="40" style="background-color: #106c3b; color: white;">Code</th>
             <th align="left" width="40" style="background-color: #106c3b; color: white;">Title</th>
             <th align="left" width="40" style="background-color: #106c3b; color: white;">Distribution Date</th>
             <th align="left" width="40" style="background-color: #106c3b; color: white;">Location</th>
             <th align="left" width="40" style="background-color: #106c3b; color: white;">Status</th>
             <th align="left" width="40" style="background-color: #106c3b; color: white;">Description</th>
-            <th align="left" width="40" style="background-color: #106c3b; color: white;">Created At</th>
+            {{-- <th align="left" width="40" style="background-color: #106c3b; color: white;">Created At</th> --}}
+            <th align="left" width="40" style="background-color: #106c3b; color: white;">Items</th>
+            <th align="left" width="40" style="background-color: #106c3b; color: white;">Supports</th>
         </tr>
     </thead>
     <tbody>
         @foreach ($distributions as $distribution)
             <tr>
-                <td align="left" width="40">{{ $distribution->id }}</td>
-                <td align="left" width="40">{{ $distribution->title }}</td>
-                <td align="left" width="40">{{ \Carbon\Carbon::parse($distribution->distribution_date)->format('F j, Y') }}</td>
-                <td align="left" width="40">{{ $distribution->location ?? 'N/A' }}</td>
-                <td align="left" width="40">{{ $distribution->status }}</td>
-                <td align="left" width="40">{{ $distribution->description ?? 'N/A' }}</td>
-                <td align="left" width="40">{{ \Carbon\Carbon::parse($distribution->created_at)->format('F j, Y H:i A') }}</td>
+                <!-- Distribution Details -->
+                <td align="left" width="40" style="vertical-align: top;">{{ $distribution->code }}</td>
+                <td align="left" width="40" style="vertical-align: top;">{{ $distribution->title }}</td>
+                <td align="left" width="40" style="vertical-align: top;">{{ \Carbon\Carbon::parse($distribution->distribution_date)->format('F j, Y') }}</td>
+                <td align="left" width="40" style="vertical-align: top;">{{ $distribution->location ?? 'N/A' }}</td>
+                <td align="left" width="40" style="vertical-align: top;">{{ $distribution->status }}</td>
+                <td align="left" width="40" style="vertical-align: top;">{{ $distribution->description ?? 'N/A' }}</td>
+                {{-- <td align="left" width="40" style="vertical-align: top;">{{ \Carbon\Carbon::parse($distribution->created_at)->format('F j, Y H:i A') }}</td> --}}
+
+                <!-- Distribution Items -->
+                <td align="left" width="40" style="vertical-align: top;">
+                    <ol>
+                        @foreach ($distribution->distributionItems as $index => $item)
+                            <li>
+                                Item Name: {{ $item->item->name ?? 'N/A' }}<br>
+                                Original Quantity: {{ $item->original_quantity }}<br>
+                                Distributed Quantity: {{ $item->quantity }}<br>
+                                Remaining Quantity: {{ $item->original_quantity - $item->quantity }}
+                            </li>
+                        @endforeach
+                    </ol>
+                </td>
+
+                <!-- Supports -->
+                <td align="left" width="40" style="vertical-align: top;">
+                    <ol>
+                        @foreach ($distribution->supports as $index => $support)
+                            <li>
+                                Name: {{ $support->personnel->user->name ?? 'N/A' }}<br>
+                                Role: {{ $support->type ?? 'N/A' }}<br>
+                                Contact: {{ $support->personnel->contact_number ?? 'N/A' }}
+                            </li>
+                        @endforeach
+                    </ol>
+                </td>
             </tr>
         @endforeach
     </tbody>
